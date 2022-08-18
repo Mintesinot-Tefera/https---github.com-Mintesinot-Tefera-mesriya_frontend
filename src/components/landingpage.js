273a1e4a92656f1a64b713d66d4117b850f43947
desmondsol <soltig66@gmail.com>
2022-08-18 14:38:18 +0300

    done landing page

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -7,7 +7,7 @@ import {
 } from "@material-tailwind/react";
 import { useNavigate, Link } from "react-router-dom";
 import home1 from "../landing/pages/Home";
-
+import App from "../landing/pages/App";
 function LandingPage() {
   // const navigate = useNavigate();
 
@@ -17,9 +17,9 @@ function LandingPage() {
 
   return (
     
-    <div className="h-screen p-20 bg-gray-300">
+    <><div className="h-screen p-20 bg-gray-300">
       <div className="grid grid-cols-2 bg-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700 px-5 py-20 h-full">
-        
+
         <div className="flex justify-center items-center gap-20 bg-gray-300">
           <a
             href="#"
@@ -30,16 +30,14 @@ function LandingPage() {
           <Link
             to="/userhome"
             className="text-sky-800 bg-gray-100 hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-          // onClick={navigateToUserhome}
-
           >
             ጀምር
           </Link>
         </div>
       </div>
-    </div>
-     
-    )
+    </div><App /></>
+
+    );
 
 }
 
